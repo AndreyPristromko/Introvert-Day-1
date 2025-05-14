82e7e9e50adb6373550681cf1d5d6306f8b78801
Andrey Pristromko <andreypristromko.uk@gmail.com>
2025-05-14 09:22:59 +0300

    Update LeadsController.php

--- src/Controller/LeadsController.php
+++ src/Controller/LeadsController.php
@@ -7,25 +7,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Service\ClientService;
-use App\Service\DealsService;
+use App\Service\LeadsService;
 
-class DealsController extends AbstractController
+class LeadsController extends AbstractController
 {
     private $clientService;
-    private $dealsService;
+    private $leadsService;
     private $logger;
 
     public function __construct(
         ClientService $clientService, 
-        DealsService $dealsService,
+        LeadsService $leadsService,
         LoggerInterface $logger
     ) {
         $this->clientService = $clientService;
-        $this->dealsService = $dealsService;
+        $this->leadsService = $leadsService;
         $this->logger = $logger;
     }
 
-    public function getDealsStatistics(Request $request): Response
+    public function getLeadsStatistics(Request $request): Response
     {
         try {
             $dateFrom = $request->query->get('date_from');
@@ -40,32 +40,31 @@ class DealsController extends AbstractController
             $statistics = [];
 
             foreach ($clients as $client) {
-                if ($result = $this->clientService->checkClients($client)) {
-                    $this->logger->info($result);
-                    $clientDealsSum = $this->dealsService->getDealsSum($dateFrom, $dateTo);
+                if ($this->clientService->checkClients($client) {
+                    $clientLeadsSum = $this->leadsService->getLeadsSum($dateFrom, $dateTo);
                     
                     $statistics[] = [
                         'id' => $client['id'],
                         'name' => $client['name'],
-                        'sum' => $clientDealsSum
+                        'sum' => $clientLeadsSum
                     ];
                     
-                    $totalSum += $clientDealsSum;
-                    $this->logger->info('Обработан клиент, доступ есть',  ['client_id' => $client['id'], 'deals_sum' => $clientDealsSum]);
+                    $totalSum += $clientLeadsSum;
+                    $this->logger->info('Обработан клиент, доступ есть',  ['client_id' => $client['id'], 'leads_sum' => $clientLeadsSum]);
                     usleep(200000);
                 } else {
-                    $clientDealsSum = 0;
+                    $clientLeadsSum = 0;
                     $statistics[] = [
                         'id' => $client['id'],
                         'name' => $client['name'],
-                        'sum' => $clientDealsSum
+                        'sum' => $clientLeadsSum
                     ];
-                    $this->logger->info('Обработан клиент, доступа нет', ['client_id' => $client['id'], 'deals_sum' => $clientDealsSum]);
+                    $this->logger->info('Обработан клиент, доступа нет', ['client_id' => $client['id'], 'leads_sum' => $clientLeadsSum]);
                     usleep(200000);
                 }
             }
 
-            return $this->render('deals/statistics.html.twig', [
+            return $this->render('leads/statistics.html.twig', [
                 'statistics' => $statistics,
                 'total_sum' => $totalSum,
                 'date_from' => date('Y-m-d H:i:s', (int)$dateFrom),
